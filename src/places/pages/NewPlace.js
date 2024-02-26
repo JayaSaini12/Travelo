7ca6c4c6a557f6c3326615f3f099018cf9751914
Jaya <jayarke890@gmail.com>
2024-02-26 22:27:44 +0530

    Adding file Upload completed

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,6 +6,7 @@ import Button from '../../shared/components/FormElements/Button';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoaingSpinner';
 import { VALIDATOR_REQUIRE,VALIDATOR_MINLENGTH} from '../../shared/util/validators';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
 import { AuthContext } from '../../shared/context/auth-context';
@@ -15,46 +16,40 @@ import './PlaceForm.css';
 const NewPlace = () => {
   const auth = useContext(AuthContext);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const[formState,inputHandler]=useForm({
-    title: {
-      value: '',
-      isValid: false
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
+      }
     },
-    description: {
-      value: '',
-      isValid: false
-    },
-    address: {
-      value: '',
-      isValid: false
-    }
-  },false);
-  // const titleInputHandler=useCallback((id,value,isValid)=>{
-  //   dispatch({type:'INPUT_CHANGE', value:value, isvalid:isvalid, inputId:id})
-  // },[]);//[] isme dependies  
+    false
+  );
 
-  // const descriptionInputHandler=useCallback((id,value,isValid)=>{},[]);
-  //make only one input hand;er to dispatch
-
-
-  
   const history = useHistory();
 
-  const placeSubmitHandler=async event=>{
-    event.preventDefault();//should not default submit the form and render the page
-    //send data to the server
+  const placeSubmitHandler = async event => {
+    event.preventDefault();
     try {
-      await sendRequest(
-        'http://localhost:5000/api/places',
-        'POST',
-        JSON.stringify({
-          title: formState.inputs.title.value,
-          description: formState.inputs.description.value,
-          address: formState.inputs.address.value,
-          creator: auth.userId
-        }),
-        { 'Content-Type': 'application/json' }
-      );
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('creator', auth.userId);
+      formData.append('image', formState.inputs.image.value);
+      await sendRequest('http://localhost:5000/api/places', 'POST', formData);
       history.push('/');
     } catch (err) {}
   };
@@ -62,45 +57,44 @@ const NewPlace = () => {
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-    <form className="place-form" onSubmit={placeSubmitHandler}>
-    {isLoading && <LoadingSpinner asOverlay />}
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
-      />
-      {/* here these are the props given to the input.js aur yrh sab at runtime user dega */}
-      {/* yaha se props leke waha input.js mai ja raha hai */}
-
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={inputHandler}
-      />
-
-      <Input
-        id="address"
-        element="input"
-        label="Address"
-        validators={[VALIDATOR_REQUIRE]}
-        errorText="Please enter a valid Address."
-        onInput={inputHandler}
-      />
-
-      <Button type="submit" disabled={!formState.isValid}>
-        ADD PLACE
-      </Button>
-    </form>
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
     </React.Fragment>
   );
 };
 
 export default NewPlace;
-
